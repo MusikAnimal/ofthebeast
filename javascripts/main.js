0357fa41492804deefb32040d66f74e12864492f
MusikAnimal <ziemba0123@gmail.com>
2015-01-18 17:20:13 -0500

    more responsiveness, imagery, styling, sammy.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-  $(".guy-melt").addClass("visible");
-})
+;(function($) {
+  app = Sammy('body', function() {
+    this.get('#/', function() {
+      $(".guy-melt").addClass("visible");
+    });
+    this.get('#/news', function() {
+      $(".news").hideClass("visible");
+    });
+    this.get('#/live', function() {
+    });
+    this.get('#/music');
+    this.get('#/join');
+
+    this.before({}, function(context) {
+      console.log(context);
+    });
+  });
+
+  $(function() {
+    app.run();
+  });
+})(jQuery);
