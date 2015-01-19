841dd6bd0a985eaf1f5806a621b5634c39dcddb4
MusikAnimal <ziemba0123@gmail.com>
2015-01-18 22:11:19 -0500

    live and music widgets, some styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,26 @@
 ;(function($) {
   app = Sammy('body', function() {
     this.get('#/', function() {
-      $(".guy-melt").addClass("visible");
+      $(".home").fadeIn(1000, function() {
+        $(".guy-melt").addClass("visible");
+      });
     });
     this.get('#/news', function() {
-      $(".news").hideClass("visible");
     });
     this.get('#/live', function() {
+      $(".live").show().find("script").replaceWith(
+        $(".live").find("script").html()
+      );
     });
-    this.get('#/music');
+    this.get('#/music', function() {
+      $(".music").show().find("script").replaceWith(
+        $(".music").find("script").html()
+      );
+    });
     this.get('#/join');
 
     this.before({}, function(context) {
-      console.log(context);
+      $("section").hide();
     });
   });
 
